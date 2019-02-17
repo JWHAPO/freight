38ea2944351f2ad5dce20579eb15481a67c08de4
JWHAPO <kim2101714@gmail.com>
2019-02-17 23:02:04 +0900

    add test UserServiceTest

--- src/test/java/app/hapo/car/freight/service/user/UserServiceTest.groovy
+++ src/test/java/app/hapo/car/freight/service/user/UserServiceTest.groovy
@@ -1,9 +1,15 @@
 package app.hapo.car.freight.service.user
 
+import app.hapo.car.freight.domain.user.Role
+import app.hapo.car.freight.domain.user.User
+import app.hapo.car.freight.domain.user.UserRepository
+import app.hapo.car.freight.domain.user.UserRole
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import spock.lang.Specification
+
+import static org.mockito.BDDMockito.*
 
 /*
  * Created by hapo
@@ -16,5 +22,25 @@ class UserServiceTest extends Specification {
     @Autowired
     UserService userService
 
-    @MockBean
+    @MockBean(name = "userRepository")
+    UserRepository userRepository
+
+    def "findById" (){
+        given:
+        long id=2
+
+        List<UserRole> userRoles = Arrays.asList(new UserRole(2L, Role.MEMBER))
+
+        given(userRepository.findById(id))
+        .willReturn(Optional.of(new User(3L,"email@email.com","1234","Mr.Park",null,null,null,null,userRoles)))
+
+
+        when:
+        Optional<User> user = userService.findById(2)
+
+        then:
+
+        user.get().password == "1234"
+
+    }
 }
