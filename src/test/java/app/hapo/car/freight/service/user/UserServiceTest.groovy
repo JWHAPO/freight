2c78851baacaefdf2e124f77c97fcfcb1fe5f635
JWHAPO <kim2101714@gmail.com>
2019-02-22 14:39:44 +0900

    ADD Test Case Of UserService save method

--- src/test/java/app/hapo/car/freight/service/user/UserServiceTest.groovy
+++ src/test/java/app/hapo/car/freight/service/user/UserServiceTest.groovy
@@ -1,9 +1,11 @@
 package app.hapo.car.freight.service.user
 
+import app.hapo.car.freight.domain.auth.email.EmailAuthRepository
 import app.hapo.car.freight.domain.user.Role
 import app.hapo.car.freight.domain.user.User
 import app.hapo.car.freight.domain.user.UserRepository
 import app.hapo.car.freight.domain.user.UserRole
+import app.hapo.car.freight.service.auth.email.EmailAuthService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
@@ -25,22 +27,44 @@ class UserServiceTest extends Specification {
     @MockBean(name = "userRepository")
     UserRepository userRepository
 
-    def "findById" (){
+    @MockBean(name = "emailAuthRepository")
+    EmailAuthRepository emailAuthRepository
+
+    @MockBean(name = "emailAuthService")
+    EmailAuthService emailAuthService
+
+    def "save"(){
         given:
-        long id=2
 
-        List<UserRole> userRoles = Arrays.asList(new UserRole(2L, Role.MEMBER))
+            List<UserRole> userRoles = Arrays.asList(new UserRole(2L, Role.MEMBER))
+            User testUser = new User(3L,"email@email.com","1234","Mr.Park",null,null,null,null,userRoles)
 
-        given(userRepository.findById(id))
-        .willReturn(Optional.of(new User(3L,"email@email.com","1234","Mr.Park",null,null,null,null,userRoles)))
-
+            given(userRepository.save(testUser))
+                    .willReturn(testUser)
 
         when:
-        Optional<User> user = userService.findById(2)
+            Optional<User> resultUser = userService.save(testUser)
 
         then:
+            resultUser.get() == testUser
 
-        user.get().password == "1234"
+    }
+
+    def "findById" (){
+        given:
+            long id=2
+
+            List<UserRole> userRoles = Arrays.asList(new UserRole(2L, Role.MEMBER))
+            User testUser = new User(3L,"email@email.com","1234","Mr.Park",null,null,null,null,userRoles)
+
+            given(userRepository.findById(id))
+            .willReturn(Optional.of(testUser))
+
+        when:
+            Optional<User> resultUser = userService.findById(2)
+
+        then:
+            resultUser.get() == testUser
 
     }
 }
