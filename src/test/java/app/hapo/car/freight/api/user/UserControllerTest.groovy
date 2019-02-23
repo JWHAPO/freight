ea92741704a3a93422c4caf8032470430da2e04a
JWHAPO <kim2101714@gmail.com>
2019-02-23 23:11:55 +0900

    explanatory user controller test

--- src/test/java/app/hapo/car/freight/api/user/UserControllerTest.groovy
+++ src/test/java/app/hapo/car/freight/api/user/UserControllerTest.groovy
@@ -18,25 +18,25 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standal
 @WebAppConfiguration
 class UserControllerTest extends Specification {
 
-    def userController = new UserController()
-
-    MockMvc mockMvc
-
-    //@Before
-    def setup(){
-        mockMvc = standaloneSetup(userController).build()
-    }
-
-    def "Test find all"(){
-        setup:
-        def token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhYWExQGVtYWludGVjLmNvbSIsInNjb3BlcyI6WyJST0xFX01FTUJFUiJdLCJpc3MiOiJoYXBvIiwiaWF0IjoxNTUwOTEyNDgxLCJleHAiOjE1NTA5MTMzODF9.jJ0554eqIGHFgTySoOklZfhk8Wu5CUSLcEiGbpKKO0yhkiiLVeHy6_xfkTIwEL5pVxB2g_of8GbJF_nowSHT1Q"
-        def request = get("/api/users")
-        request.header("Authorization","Bearer $token")
-
-        when:
-        def response = mockMvc.perform(get("/api/users"))
-
-        then:
-        response.andExpect(status().isOk())
-    }
+//    def userController = new UserController()
+//
+//    MockMvc mockMvc
+//
+//    //@Before
+//    def setup(){
+//        mockMvc = standaloneSetup(userController).build()
+//    }
+//
+//    def "Test find all"(){
+//        setup:
+//        def token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhYWExQGVtYWludGVjLmNvbSIsInNjb3BlcyI6WyJST0xFX01FTUJFUiJdLCJpc3MiOiJoYXBvIiwiaWF0IjoxNTUwOTEyNDgxLCJleHAiOjE1NTA5MTMzODF9.jJ0554eqIGHFgTySoOklZfhk8Wu5CUSLcEiGbpKKO0yhkiiLVeHy6_xfkTIwEL5pVxB2g_of8GbJF_nowSHT1Q"
+//        def request = get("/api/users")
+//        request.header("Authorization","Bearer $token")
+//
+//        when:
+//        def response = mockMvc.perform(get("/api/users"))
+//
+//        then:
+//        response.andExpect(status().isOk())
+//    }
 }
