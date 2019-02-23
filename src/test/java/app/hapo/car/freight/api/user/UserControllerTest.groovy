2ae1e055e80391e5f690642beff751eac15c838d
JWHAPO <kim2101714@gmail.com>
2019-02-23 18:30:23 +0900

    change user controller test... not completed

--- src/test/java/app/hapo/car/freight/api/user/UserControllerTest.groovy
+++ src/test/java/app/hapo/car/freight/api/user/UserControllerTest.groovy
@@ -1,22 +1,13 @@
 package app.hapo.car.freight.api.user
 
-import app.hapo.car.freight.WebSecurityConfig
-import app.hapo.car.freight.service.user.UserService
-import app.hapo.car.freight.service.usercar.UserCarService
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.context.runner.WebApplicationContextRunner
-import org.springframework.test.context.ContextConfiguration
+
 import org.springframework.test.context.web.WebAppConfiguration
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.web.context.WebApplicationContext
 import spock.lang.Specification
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity
 
 /**
  * freight
@@ -24,35 +15,23 @@ import static org.springframework.security.test.web.servlet.setup.SecurityMockMv
  * Created by hapo on 2019-02-22.
  * Description: 
  */
-@ContextConfiguration(classes = [UserController, WebSecurityConfig])
 @WebAppConfiguration
 class UserControllerTest extends Specification {
-
-    @Autowired
-    WebApplicationContext context
 
     def userController = new UserController()
 
     MockMvc mockMvc
 
-    @Autowired
-    UserResourceAssembler userResourceAssembler
-
-    @Autowired
-    UserService userService
-
-    @Autowired
-    UserCarService userCarService
-
     //@Before
     def setup(){
-        mockMvc = webAppContextSetup(context).apply(springSecurity()).build()
+        mockMvc = standaloneSetup(userController).build()
     }
 
     def "Test find all"(){
         setup:
+        def token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhYWExQGVtYWludGVjLmNvbSIsInNjb3BlcyI6WyJST0xFX01FTUJFUiJdLCJpc3MiOiJoYXBvIiwiaWF0IjoxNTUwOTEyNDgxLCJleHAiOjE1NTA5MTMzODF9.jJ0554eqIGHFgTySoOklZfhk8Wu5CUSLcEiGbpKKO0yhkiiLVeHy6_xfkTIwEL5pVxB2g_of8GbJF_nowSHT1Q"
         def request = get("/api/users")
-        request.param("name","Kim")
+        request.header("Authorization","Bearer $token")
 
         when:
         def response = mockMvc.perform(get("/api/users"))
