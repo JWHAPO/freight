0f7b64974420d20e289df45205df6cc4e9ed131e
JWHAPO <kim2101714@gmail.com>
2019-03-07 01:13:44 +0900

    upload model add

---
+++ src/main/java/app/hapo/car/freight/common/upload/FileStorageProperties.java
@@ -0,0 +1,16 @@
+package app.hapo.car.freight.common.upload;/*
+ * Created by hapo
+ * Date : 19. 3. 7 오전 12:32
+ * Description :
+ */
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "file")
+public class FileStorageProperties {
+    private String uploadDir;
+}
