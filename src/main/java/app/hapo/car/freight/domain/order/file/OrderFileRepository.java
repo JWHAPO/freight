b28503d076d8322e3a115061e8e67fe37db0e5d8
JWHAPO <kim2101714@gmail.com>
2019-03-09 21:26:23 +0900

    add order file repository

---
+++ src/main/java/app/hapo/car/freight/domain/order/file/OrderFileRepository.java
@@ -0,0 +1,10 @@
+package app.hapo.car.freight.domain.order.file;/*
+ * Created by hapo
+ * Date : 19. 3. 9 오후 7:09
+ * Description :
+ */
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderFileRepository extends JpaRepository<OrderFile,Long> {
+}
