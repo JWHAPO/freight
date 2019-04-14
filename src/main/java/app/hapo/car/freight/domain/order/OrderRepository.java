0cb1ddac71aac5c4930246e95b37f0ddc6c5a011
JWHAPO <kim2101714@gmail.com>
2019-04-15 00:10:00 +0900

    change return type

--- src/main/java/app/hapo/car/freight/domain/order/OrderRepository.java
+++ src/main/java/app/hapo/car/freight/domain/order/OrderRepository.java
@@ -14,7 +14,7 @@ import java.util.List;
  */
 public interface OrderRepository extends JpaRepository<Order,Long> {
 
-    Long countByStatus(OrderStatus status);
+    int countByStatus(OrderStatus status);
 
     Page<Order> findByStatus(OrderStatus status, Pageable pageable);
 }
